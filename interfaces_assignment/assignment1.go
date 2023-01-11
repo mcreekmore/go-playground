2d519c0725e2dacf18b8a3a4b20d1da35b91ac1a
Matt Creekmore <matthewacreekmore@gmail.com>
2023-01-11 07:38:17 -0500

    update

--- interfaces_assignment/assignment1.go
+++ interfaces_assignment/assignment1.go
@@ -1,43 +1,43 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// type square struct {
-// 	height float64
-// 	width  float64
-// }
+type square struct {
+	height float64
+	width  float64
+}
 
-// type triangle struct {
-// 	base   float64
-// 	height float64
-// }
+type triangle struct {
+	base   float64
+	height float64
+}
 
-// type shape interface {
-// 	getArea() float64
-// }
+type shape interface {
+	getArea() float64
+}
 
-// func main() {
-// 	s := square{
-// 		height: 10,
-// 		width:  10,
-// 	}
-// 	t := triangle{
-// 		base:   15,
-// 		height: 8,
-// 	}
+func main() {
+	s := square{
+		height: 10,
+		width:  10,
+	}
+	t := triangle{
+		base:   15,
+		height: 8,
+	}
 
-// 	printArea(s)
-// 	printArea(t)
-// }
+	printArea(s)
+	printArea(t)
+}
 
-// func (s square) getArea() float64 {
-// 	return s.height * s.width
-// }
+func (s square) getArea() float64 {
+	return s.height * s.width
+}
 
-// func (t triangle) getArea() float64 {
-// 	return .5 * t.base * t.height
-// }
+func (t triangle) getArea() float64 {
+	return .5 * t.base * t.height
+}
 
-// func printArea(s shape) {
-// 	fmt.Println(s.getArea())
-// }
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
